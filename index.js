62d605200c913a1ebb3631552a009ca2b32da880
Kyle <kylewilkinson14@gmail.com>
2021-10-14 17:10:30 +1100

    completed tests

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+const hqLocation = 42
+let distanceFromHqInBlocks = (currentBlock) => {
+    if(currentBlock < hqLocation)
+       return hqLocation - currentBlock
+     else if(currentBlock > hqLocation)
+        return currentBlock - hqLocation
+}
+
+console.log(distanceFromHqInBlocks(43))
+
+const blockInFeet = 264
+let distanceFromHqInFeet = (currentBlock) => {
+    return distanceFromHqInBlocks(currentBlock) * blockInFeet
+}
+
+console.log(distanceFromHqInFeet(43))
+
+let distanceTravelledInFeet = (start, destination) => {
+    if(start < destination)
+       return (destination - start) * blockInFeet
+     else if(start > destination)
+        return (start - destination) * blockInFeet
+}
+
+console.log(distanceTravelledInFeet(50, 60))
+
+let calculatesFarePrice = (start, destination) => {
+    if(distanceTravelledInFeet(start, destination) >= 2500)
+        return 'cannot travel that far'
+    else if(distanceTravelledInFeet(start, destination) < 400)
+        return 0
+        else if(distanceTravelledInFeet(start, destination) <= 2000)
+            return (distanceTravelledInFeet(start, destination) - 400) * 0.02
+            else if(distanceTravelledInFeet(start, destination) > 2000)
+                return 25
+}
+
+console.log(calculatesFarePrice(34, 24))
